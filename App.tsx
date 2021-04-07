93211bb1042765f5281a61aa8c0d10b2653ae085
Matheus Santos <matheus_sa_santos@hotmail.com>
2021-04-07 19:44:31 -0300

    adding libraries

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-
+import {NavigationContainer} from '@react-navigation/native';
 EStyleSheet.build();
 
 import HomeScreen from './src/pages/HomeScreen';
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
+      NavigationContainer
       <HomeScreen />
     </SafeAreaView>
   );
