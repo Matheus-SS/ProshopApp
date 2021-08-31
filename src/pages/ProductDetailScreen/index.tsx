e920069ab422b46970fc0489233f03b378bc339f
Matheus Santos <matheus_sa_santos@hotmail.com>
2021-08-30 21:35:25 -0300

    adicionando husky

--- src/pages/ProductDetailScreen/index.tsx
+++ src/pages/ProductDetailScreen/index.tsx
@@ -41,6 +41,7 @@ type ProductDetailScreenRouteProp = RouteProp<ParamList, 'ProductDetail'>;
 const ProductDetailScreen = ({navigation}: IHomeProps) => {
   const {item} = useRoute<ProductDetailScreenRouteProp>().params;
 
+  const tre = 'te';
   const navigateToCart = useCallback(() => {
     navigation.navigate('Cart');
   }, [navigation]);
